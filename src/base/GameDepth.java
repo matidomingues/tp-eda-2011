a81bcb7a8813e1e4ef5f9e0faf55725457b3a2fb
Matias Domingues <mannias@gmail.com>
2011-10-25 16:12:49 -0300

    commit before merge

--- src/base/GameDepth.java
+++ src/base/GameDepth.java
@@ -1,9 +1,11 @@
 package base;
 
-import MinMax.PointCalc;
+import testbiz.GraphViz;
 
 public class GameDepth extends Game {
 
+	GraphViz vz = new GraphViz();
+	
 	public GameDepth(String filePath, int depth) throws Exception {
 		this.heuristic = this.createHeuristic();
 		if (filePath == null) {
@@ -15,29 +17,37 @@ public class GameDepth extends Game {
 	}
 	
 	public Point miniMax(Board board, int depth, Cell player){
+		vz.addln(vz.start_graph());
 		Point point = null;
 		int euristic = Integer.MIN_VALUE;
 		for(Point p: board.moves(player).keySet()){
 			Board child = board.clone();
+			vz.addln( board.hashCode() + " -> "+child.hashCode());
 			child.add(p.getX(), p.getY(), player);
-			int euristicPoint = minimax(board,depth-1,Integer.MIN_VALUE,Integer.MIN_VALUE, player.oposite());
+			int euristicPoint = minimax(child,depth-1,Integer.MIN_VALUE,Integer.MIN_VALUE, player.oposite());
+			vz.addln(child.hashCode() + " [label = \"" + p + euristicPoint+ "\"];");
 			if(euristic <= euristicPoint){
 				point = p;
 			}			
 		}
+		vz.addln(board.hashCode() + " [label = \"null " + euristic + "\"];");
+		vz.addln(vz.end_graph());
+		System.out.println(vz.getDotSource());
 		return point;		
 		
 	}
 	
 	private int minimax(Board board,int depth,int alpha,int beta, Cell player){
 		if(depth == 0 || gameEnded(board)){
-			board.evaluateBoard(player);
+			return board.evaluateBoard(player);
 		}
 		if(player == currentPlayer){
 			for(Point p: board.moves(player).keySet()){
 				Board child = board.clone();
+				vz.addln(board.hashCode() + " -> "+ child.hashCode());
 				child.add(p.getX(), p.getY(), player);
 				alpha = Math.max(alpha, minimax(child,depth-1,alpha,beta,player.oposite()));
+				vz.addln(child.hashCode() + " [label = \"" + p + alpha+ "\"];");
 				if(prune && (beta <= alpha)){
 					break;
 				}
@@ -47,8 +57,10 @@ public class GameDepth extends Game {
 		else{
 			for(Point p: board.moves(player).keySet()){
 				Board child = board.clone();
+				vz.addln(board.hashCode() + " -> "+ child.hashCode());
 				child.add(p.getX(), p.getY(), player);
 				beta = Math.min(beta, minimax(child,depth-1,alpha,beta,player.oposite()));
+				vz.addln(child.hashCode() + " [label = \"" + p + beta + "\"];");
 				if(prune && (beta <= alpha)){
 					break;
 				}
